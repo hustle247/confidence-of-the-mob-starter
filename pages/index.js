8d2ea8065c708ebfb5efc9c00ca04e84dc97db49
Eddy Inserra <eddyinserra@gmail.com>
2026-02-25 21:20:07 -0500

    Update homepage button emoji to books (📚)

--- pages/index.js
+++ pages/index.js
@@ -44,7 +44,7 @@ export default function Home() {
           <div className="mt-10 flex flex-col sm:flex-row justify-center space-y-4 sm:space-y-0 sm:space-x-6">
             <Link href="/buy-book" legacyBehavior>
               <a className="cta-button bg-accent-red text-white font-bold py-3 px-8 rounded-lg shadow-file hover:bg-red-600 transition duration-300">
-                🎧 Order The Book
+                📚 Order The Book
               </a>
             </Link>
             {/* <Link href="/files" legacyBehavior>
