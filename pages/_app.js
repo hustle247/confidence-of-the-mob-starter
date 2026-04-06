3c152d8b0487ef280f2ef17688ef1403df7dad3f
Eddy Inserra <eddyinserra@gmail.com>
2026-04-06 09:06:51 -0400

    Install Meta Pixel

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Script from 'next/script';
 import '../styles/globals.css';
 import Layout from '../components/Layout';
 import { Analytics } from "@vercel/analytics/next";
@@ -11,7 +12,34 @@ function MyApp({ Component, pageProps }) {
         <link rel="apple-touch-icon" href="/favicon.png" />
         <link rel="manifest" href="/site.webmanifest" />
       </Head>
+      <Script
+        id="fb-pixel"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1268456198274255');
+            fbq('track', 'PageView');
+          `,
+        }}
+      />
       <Component {...pageProps} />
+      <noscript>
+        <img
+          height="1"
+          width="1"
+          style={{ display: 'none' }}
+          src="https://www.facebook.com/tr?id=1268456198274255&ev=PageView&noscript=1"
+          alt=""
+        />
+      </noscript>
       <Analytics />
     </Layout>
   );
